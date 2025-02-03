6a10f86ceecbc9bdee65b336719e064f3e7a6162
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-02-03 16:32:37 -0600

    feat(users): crear endpoint de findAll

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>
+  ) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(): Promise<User[]> {
+    try {
+      return await this.userModel.find().exec();
+    } catch (error: any) {
+      throw new HttpException(`Error al obtener los usuarios: ${error.message}`, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   findOne(id: number) {
