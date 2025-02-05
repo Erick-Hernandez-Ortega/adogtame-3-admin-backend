c2083505aa4fd2390a6d31539db0fa92e04e0f3a
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-02-04 21:02:44 -0600

    feat: agregar swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config: Omit<OpenAPIObject, "paths"> = new DocumentBuilder()
+    .setTitle('API Adogtame')
+    .setDescription('Documentación de la API de administrador de Adogtame')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+    const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
