e08f4e283261103db3803c93b425e064b4a98a93
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-02-05 21:47:08 -0600

    feat(users): agregar validaciones en el DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,18 +1,36 @@
 import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty, IsNumberString, MaxLength, MinLength } from "class-validator";
 
 export class CreateUserDto {
     @ApiProperty({ example: 'Erick', description: 'Nombre del usuario' })
+    @IsNotEmpty()
+    @MinLength(2)
+    @MaxLength(40)
     name: string;
 
     @ApiProperty({ example: 'erick123', description: 'Nombre de usuario' })
+    @IsNotEmpty()
+    @MinLength(2)
+    @MaxLength(15)
     username: string;
 
     @ApiProperty({ example: '25', description: 'Edad del usuario' })
+    @IsNotEmpty()
+    @IsNumberString()
+    @MinLength(1)
+    @MaxLength(3)
     age: string;
 
     @ApiProperty({ example: 'erick@mail.com', description: 'Correo electrónico' })
+    @IsNotEmpty()
+    @IsEmail()
+    @MaxLength(20)
+    @MinLength(3)
     email: string;
 
+    @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(20)
     @ApiProperty({ example: 'securepassword', description: 'Contraseña del usuario' })
     password: string;
 
