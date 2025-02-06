490cd8b77b49e8c5ea24d51aea53e30c27751e03
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-02-05 21:25:56 -0600

    feat(users): crear DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,27 @@
-export class CreateUserDto {}
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+
+export class CreateUserDto {
+    @ApiProperty({ example: 'Erick', description: 'Nombre del usuario' })
+    name: string;
+
+    @ApiProperty({ example: 'erick123', description: 'Nombre de usuario' })
+    username: string;
+
+    @ApiProperty({ example: '25', description: 'Edad del usuario' })
+    age: string;
+
+    @ApiProperty({ example: 'erick@mail.com', description: 'Correo electrónico' })
+    email: string;
+
+    @ApiProperty({ example: 'securepassword', description: 'Contraseña del usuario' })
+    password: string;
+
+    @ApiPropertyOptional({ example: 'default.png', description: 'Imagen de perfil' })
+    imagen?: string;
+
+    @ApiPropertyOptional({ example: false, description: 'Indica si el token fue eliminado' })
+    isTokenRemoved?: boolean;
+
+    @ApiPropertyOptional({ example: new Date(), description: 'Fecha de creación' })
+    date?: Date;
+}
