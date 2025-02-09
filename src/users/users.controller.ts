2b58cdd34b8d4eeccf151139478bf09434b4c8c0
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-02-08 22:44:41 -0600

    docs(users): agregar descripcion a endpoints faltantes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,6 +11,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Crear un nuevo usuario' })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
@@ -28,11 +29,13 @@ export class UsersController {
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Actualizar un usuario por su ID' })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Borrar un nuevo usuario por su ID' })
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
