be8145ba41aa164dee6573056e658f967bc38567
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-02-11 21:34:50 -0600

    docs(pets): agregar descripcion de los endpoints

--- src/pets/pets.controller.ts
+++ src/pets/pets.controller.ts
@@ -2,32 +2,39 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { PetsService } from './pets.service';
 import { CreatePetDto } from './dto/create-pet.dto';
 import { UpdatePetDto } from './dto/update-pet.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Pets')
 @Controller('pets')
 export class PetsController {
   constructor(private readonly petsService: PetsService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Crear una nueva mascota' })
   create(@Body() createPetDto: CreatePetDto) {
     return this.petsService.create(createPetDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Obtener todas las mascotas' })
   findAll() {
     return this.petsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Obtener una mascota por ID' })
   findOne(@Param('id') id: string) {
-    return this.petsService.findOne(+id);
+    return this.petsService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Actualizar una mascota por ID' })
   update(@Param('id') id: string, @Body() updatePetDto: UpdatePetDto) {
     return this.petsService.update(+id, updatePetDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Borrar una mascota por ID' })
   remove(@Param('id') id: string) {
     return this.petsService.remove(+id);
   }
