e9574cfbb8e0650b03eb5f9963dcb0686a261abd
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-02-12 21:06:47 -0600

    feat(pets): importar modelo de user

--- src/pets/pets.module.ts
+++ src/pets/pets.module.ts
@@ -3,10 +3,11 @@ import { PetsService } from './pets.service';
 import { PetsController } from './pets.controller';
 import { Pet, PetSchema } from './schemas/pet.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/users/schemas/user.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Pet.name, schema: PetSchema }]),
+    MongooseModule.forFeature([{ name: Pet.name, schema: PetSchema }, { name: User.name, schema: UserSchema }]),
   ],
   controllers: [PetsController],
   providers: [PetsService],
