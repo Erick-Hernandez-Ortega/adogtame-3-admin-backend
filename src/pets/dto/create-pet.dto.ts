460ff2924b83df00b52b2db9b10f4f585e37c4f0
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-02-12 21:06:31 -0600

    feat(pets): crear DTO de create

--- src/pets/dto/create-pet.dto.ts
+++ src/pets/dto/create-pet.dto.ts
@@ -1,1 +1,47 @@
-export class CreatePetDto {}
+import { Type } from "class-transformer";
+import { IsBoolean, IsMongoId, IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreatePetDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    breed: string;
+
+    @IsString()
+    @IsNotEmpty()
+    age: string;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsBoolean()
+    @IsNotEmpty()
+    stirilized: boolean;
+
+    @IsString()
+    @IsNotEmpty()
+    sex: string;
+
+    @IsString()
+    @IsNotEmpty()
+    typeOfPet: string;
+
+    @IsString()
+    @IsNotEmpty()
+    size: string;
+
+    @IsBoolean()
+    @IsOptional()
+    available?: boolean;
+
+    @IsMongoId()
+    @IsNotEmpty()
+    owner: string;
+
+    @IsOptional()
+    image?: string;
+}
