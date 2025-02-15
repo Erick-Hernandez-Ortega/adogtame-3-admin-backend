cd8a4844ee45ea6918d6b3adc8f544a635bf449b
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-02-14 19:45:35 -0600

    feat(adopcions): importar schemas de mongo

--- src/adopcions/adopcions.module.ts
+++ src/adopcions/adopcions.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AdopcionsService } from './adopcions.service';
 import { AdopcionsController } from './adopcions.controller';
+import { Adopcion, AdoptionSchema } from './schemas/adopcion.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pet, PetSchema } from 'src/pets/schemas/pet.schema';
+import { User, UserSchema } from 'src/users/schemas/user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Adopcion.name, schema: AdoptionSchema },
+      { name: Pet.name, schema: PetSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
   controllers: [AdopcionsController],
   providers: [AdopcionsService],
 })
