237522be6bfb86f7315dbdd1d7ca26242be4aaf4
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-02-14 19:21:11 -0600

    feat(adopcions): crear schema

--- src/adopcions/schemas/adopcion.schema.ts
+++ src/adopcions/schemas/adopcion.schema.ts
@@ -1,1 +1,26 @@
-export class Adopcion {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, Types } from "mongoose";
+
+@Schema({ timestamps: true, collection: 'adopcions' })
+export class Adopcion extends Document {
+    @Prop({ type: Types.ObjectId, ref: 'Pet', required: true })
+    petId: Types.ObjectId;
+
+    @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+    userId: Types.ObjectId;
+
+    @Prop({
+        type: String,
+        enum: ['pending', 'approved', 'rejected', 'completed'],
+        required: true
+    })
+    status: string;
+
+    @Prop({ type: String, default: null })
+    reason?: string;
+
+    @Prop({ type: String, default: null })
+    comments?: string
+}
+
+export const AdoptionSchema = SchemaFactory.createForClass(Adopcion);
