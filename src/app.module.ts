ee1c4b6f5b11c1ccd648f799b9d6bec1fe68ee93
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-02-14 20:12:52 -0600

    feat(admins): crear resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { PetsModule } from './pets/pets.module';
 import { AdopcionsModule } from './adopcions/adopcions.module';
+import { AdminsModule } from './admins/admins.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { AdopcionsModule } from './adopcions/adopcions.module';
     UsersModule,
     PetsModule,
     AdopcionsModule,
+    AdminsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
