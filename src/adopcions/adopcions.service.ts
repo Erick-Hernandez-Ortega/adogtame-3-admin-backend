abc12ffc09c5f119af5c00d4dc45a10c1f7d8aaf
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-02-14 19:46:45 -0600

    feat(adopcions): crear endpoint de crear

--- src/adopcions/adopcions.service.ts
+++ src/adopcions/adopcions.service.ts
@@ -1,11 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateAdopcionDto } from './dto/create-adopcion.dto';
 import { UpdateAdopcionDto } from './dto/update-adopcion.dto';
+import { Model } from 'mongoose';
+import { Adopcion } from './schemas/adopcion.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from 'src/users/schemas/user.schema';
+import { Pet } from 'src/pets/schemas/pet.schema';
 
 @Injectable()
 export class AdopcionsService {
-  create(createAdopcionDto: CreateAdopcionDto) {
-    return 'This action adds a new adopcion';
+  constructor(
+    @InjectModel(Adopcion.name) private readonly adopcionModel: Model<Adopcion>,
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+    @InjectModel(Pet.name) private readonly petModel: Model<Pet>
+  ) {}
+
+  async create(createAdopcionDto: CreateAdopcionDto): Promise<any> {
+    const pet: Pet = await this.petModel.findById(createAdopcionDto.petId).exec();
+    const user: User = await this.userModel.findById(createAdopcionDto.userId).exec();
+
+    if (!pet) throw new HttpException('No se encontró la mascota', HttpStatus.NOT_FOUND);
+    if (!user) throw new HttpException('No se encontró el usuario', HttpStatus.NOT_FOUND);
+
+    try {
+      const adopcion: Adopcion = new this.adopcionModel(createAdopcionDto);
+      await adopcion.save();
+
+      return { message: 'Adopcion creada correctamente' };
+    } catch (error: any) {
+      throw new HttpException(`Error al crear la adopcion: ${error.message}`, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   findAll() {
