c951e4f06d643f584a8313a900dd1630f7a670fb
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-02-14 19:21:44 -0600

    feat(adopcions): crear DTO

--- src/adopcions/dto/create-adopcion.dto.ts
+++ src/adopcions/dto/create-adopcion.dto.ts
@@ -1,1 +1,23 @@
-export class CreateAdopcionDto {}
+import { IsEnum, IsMongoId, IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreateAdopcionDto {
+    @IsMongoId()
+    @IsNotEmpty()
+    petId: string;
+
+    @IsMongoId()
+    @IsNotEmpty()
+    userId: string;
+
+    @IsEnum(['pending', 'approved', 'rejected', 'completed'])
+    @IsNotEmpty()
+    status: string;
+
+    @IsString()
+    @IsOptional()
+    reason?: string;
+
+    @IsString()
+    @IsOptional()
+    comments?: string;
+}
