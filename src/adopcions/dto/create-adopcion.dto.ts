a76fa17722aef14a41dbcac5e45a7b9abe3b0526
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-02-16 20:05:31 -0600

    docs(adoption): agregar descripcion al dto

--- src/adopcions/dto/create-adopcion.dto.ts
+++ src/adopcions/dto/create-adopcion.dto.ts
@@ -1,23 +1,29 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEnum, IsMongoId, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class CreateAdopcionDto {
     @IsMongoId()
+    @ApiProperty({ example: '61d2a0a0a0a0a0a0a0a0a0a', description: 'Id de la mascota' })
     @IsNotEmpty()
     petId: string;
 
     @IsMongoId()
+    @ApiProperty({ example: '61d2a0a0a0a0a0a0a0a0a0a', description: 'Id del usuario' })
     @IsNotEmpty()
     userId: string;
 
     @IsEnum(['pending', 'approved', 'rejected', 'completed'])
+    @ApiProperty({ example: 'pending', description: 'Estado de la adopcion' })
     @IsNotEmpty()
     status: string;
 
     @IsString()
+    @ApiProperty({ example: 'El quiere un perro', description: 'Motivo de la adopcion' })
     @IsOptional()
     reason?: string;
 
     @IsString()
+    @ApiProperty({ example: 'Es un buen perfil para la adopcion', description: 'Comentarios de la adopcion por parte de un admin' })
     @IsOptional()
     comments?: string;
 }
