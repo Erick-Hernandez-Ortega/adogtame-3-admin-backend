33e32a447e333b090e5dec3db621265a25e55562
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-02-16 20:29:37 -0600

    docs(admin): agregar descripcion de los endpoints

--- src/admins/admins.controller.ts
+++ src/admins/admins.controller.ts
@@ -2,32 +2,39 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { AdminsService } from './admins.service';
 import { CreateAdminDto } from './dto/create-admin.dto';
 import { UpdateAdminDto } from './dto/update-admin.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Admins')
 @Controller('admins')
 export class AdminsController {
   constructor(private readonly adminsService: AdminsService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Crear un nuevo administrador' })
   create(@Body() createAdminDto: CreateAdminDto) {
     return this.adminsService.create(createAdminDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Obtener todos los administradores' })
   findAll() {
     return this.adminsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Obtener un administrador por ID' })
   findOne(@Param('id') id: string) {
     return this.adminsService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Actualizar un administrador por ID' })
   update(@Param('id') id: string, @Body() updateAdminDto: UpdateAdminDto) {
     return this.adminsService.update(+id, updateAdminDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Eliminar un administrador por ID' })
   remove(@Param('id') id: string) {
     return this.adminsService.remove(+id);
   }
