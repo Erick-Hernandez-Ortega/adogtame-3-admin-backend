a844cc26bbfc22d9441c96dadc36d86d2e258c0d
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-02-16 20:12:06 -0600

    feat(adoption): crear schema

--- src/admins/admins.module.ts
+++ src/admins/admins.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AdminsService } from './admins.service';
 import { AdminsController } from './admins.controller';
+import { Admin, AdminSchema } from './schemas/admin.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Admin.name, schema: AdminSchema },
+    ])
+  ],
   controllers: [AdminsController],
   providers: [AdminsService],
 })
--- src/admins/schemas/admin.schema.ts
+++ src/admins/schemas/admin.schema.ts
@@ -1,1 +1,22 @@
-export class Admin {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema({ timestamps: true })
+export class Admin extends Document {
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ required: true })
+    email: string;
+
+    @Prop({ required: true })
+    password: string;
+
+    @Prop({ default: 'default.png' })
+    profilePicture: string;
+
+    @Prop({ default: false })
+    isTokenRemoved: boolean;
+}
+
+export const AdminSchema = SchemaFactory.createForClass(Admin);
