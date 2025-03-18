49bf771c7da212a84f88fcd94dd0916851c2de13
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-03-17 21:19:14 -0600

    feat(publications): modificar create publication DTO

--- src/publications/dto/create-publication.dto.ts
+++ src/publications/dto/create-publication.dto.ts
@@ -1,1 +1,31 @@
-export class CreatePublicationDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEnum, IsMongoId, IsNotEmpty, isString, IsString } from "class-validator";
+
+export class CreatePublicationDto {
+    @IsMongoId()
+    @ApiProperty({ example: '61d2a0a0a0a0a0a0a0a0a0a', description: 'Id del dueno' })
+    @IsNotEmpty()
+    ownerId: string;
+
+    @IsMongoId()
+    @ApiProperty({ example: '61d2a0a0a0a0a0a0a0a0a0a', description: 'Id de la mascota' })
+    @IsNotEmpty()
+    petId: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @ApiProperty({ example: 'Lindo perro para adoptar', description: 'Titulo de la publicacion' })
+    title: string;
+
+    @IsString()
+    @ApiProperty({ example: 'Perro de 3 años...', description: 'Descripcion de la publicacion' })
+    description: string;
+
+    @IsEnum(['created', 'archived', 'completed', 'rejected', 'approved'])
+    @ApiProperty({ example: 'created', description: 'Estado de la adopcion' })
+    @IsNotEmpty()
+    status: string;
+
+    @ApiProperty({ example: ['61d2a0a0a0a0a0a0a0a0a0a', '61d2a0a0a0a0a0a0a0a0a0a'], description: 'Id de los adoptantes interesados' })
+    adoptersIds: string[]
+}
