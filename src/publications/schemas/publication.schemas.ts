bb7933fefed815eec7a2e7129e90b35a71fcd327
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-03-19 14:52:32 -0600

    feat(publications): crear schema

--- src/publications/schemas/publication.schemas.ts
+++ src/publications/schemas/publication.schemas.ts
@@ -1,1 +1,26 @@
-export class Publication {}
+import { Prop, Schema } from "@nestjs/mongoose";
+import { Document, Types } from "mongoose";
+
+@Schema({ timestamps: true, collection: 'publications', versionKey: false })
+export class Publication extends Document {
+    @Prop({ type: Types.ObjectId, ref: 'users', required: true })
+    ownerId: Types.ObjectId;
+    
+    @Prop({ type: Types.ObjectId, ref: 'mascotas', required: true })
+    petId: Types.ObjectId;
+
+    @Prop({ type: String, required: true })
+    title: string;
+
+    @Prop({ type: String, required: false })
+    description: string;
+
+    @Prop({ type: String, enum: ['created', 'archived', 'completed', 'rejected', 'approved'], required: true })
+    status: string;
+
+    @Prop({ type: [Types.ObjectId], ref: 'users', required: false })
+    adoptersIds: Types.ObjectId[];
+
+    @Prop({ type: String, required: false })
+    comments: string;
+}
