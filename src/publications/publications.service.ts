44174259f53df6f7e4c4cf2bb482af5ed01cd1e2
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-03-19 15:15:39 -0600

    feat(publications): agregar endpoint de crear

--- src/publications/publications.service.ts
+++ src/publications/publications.service.ts
@@ -1,11 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreatePublicationDto } from './dto/create-publication.dto';
 import { UpdatePublicationDto } from './dto/update-publication.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Publication } from './schemas/publication.schemas';
+import { Model } from 'mongoose';
+import { User } from 'src/users/schemas/user.schema';
+import { Pet } from 'src/pets/schemas/pet.schema';
 
 @Injectable()
 export class PublicationsService {
-  create(createPublicationDto: CreatePublicationDto) {
-    return 'This action adds a new publication';
+  constructor(
+    @InjectModel(Publication.name) private readonly publicationModel: Model<Publication>,
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+    @InjectModel(Pet.name) private readonly petModel: Model<Pet>
+  ) {}
+
+  async create(createPublicationDto: CreatePublicationDto) {
+    const pet: Pet = await this.petModel.findById(createPublicationDto.petId).exec();
+    const user: User = await this.userModel.findById(createPublicationDto.ownerId).exec();
+
+    if (!pet) throw new HttpException('No se encontró la mascota', HttpStatus.NOT_FOUND);
+    if (!user) throw new HttpException('No se encontró el usuario owner', HttpStatus.NOT_FOUND);
+
+    if (createPublicationDto.ownerId !== pet.owner.toString()) 
+      throw new HttpException('El dueño de la mascota no coincide con el dueño de la publicación', HttpStatus.CONFLICT);
+
+    try {
+      createPublicationDto.status = 'created';
+      await this.publicationModel.create(createPublicationDto);
+
+      return { message: 'Publicación creada correctamente' };
+    } catch (error: any) {
+      throw new HttpException(`Error al crear la publicación: ${error.message}`, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   findAll() {
