2ec7b319f31ff20bc96d4a6da485685e3d4d388d
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-03-19 15:25:12 -0600

    fix(publications): cambiar el nombre de ref

--- src/publications/schemas/publication.schemas.ts
+++ src/publications/schemas/publication.schemas.ts
@@ -1,12 +1,14 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document, Types } from "mongoose";
+import { Pet } from "src/pets/schemas/pet.schema";
+import { User } from "src/users/schemas/user.schema";
 
 @Schema({ timestamps: true, collection: 'publications', versionKey: false })
 export class Publication extends Document {
-    @Prop({ type: Types.ObjectId, ref: 'users', required: true })
+    @Prop({ type: Types.ObjectId, ref: User.name, required: true })
     ownerId: Types.ObjectId;
     
-    @Prop({ type: Types.ObjectId, ref: 'mascotas', required: true })
+    @Prop({ type: Types.ObjectId, ref: Pet.name, required: true })
     petId: Types.ObjectId;
 
     @Prop({ type: String, required: true })
@@ -18,7 +20,7 @@ export class Publication extends Document {
     @Prop({ type: String, enum: ['created', 'archived', 'completed', 'rejected', 'approved'], required: true })
     status: string;
 
-    @Prop({ type: [Types.ObjectId], ref: 'users', required: false })
+    @Prop({ type: [Types.ObjectId], ref: User.name, required: false })
     adoptersIds: Types.ObjectId[];
 
     @Prop({ type: String, required: false })
